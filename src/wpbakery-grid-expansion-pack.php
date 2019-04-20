88eec5b71c0423dd14ed07798865b21179ddb837
Tron307020 <mike@sybilmedia.com>
2019-04-19 23:25:42 -0500

    Add files via upload

---
+++ src/wpbakery-grid-expansion-pack.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: WP Bakery Grid Expansion Pack
+ * Plugin URI: http://www.sybilmedia.com/wpbakery-grid-expansion-pack
+ * Description: Expansion pack for the Wordpress Bakery Post/Mediagrid elements
+ * Version: 1.0
+ * Author: Michael Castro
+ * Author URI: http://www.sybilmedia.com
+ */
+?>
